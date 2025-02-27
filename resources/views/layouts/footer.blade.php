cc0761996332bd1f5158196abb7c7f8b7dc49fcd
unknown <nayakaaabii2006@gmail.com>
2025-02-28 06:44:39 +0700

    pertama

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2025 &copy; Master</p>
+        </div>
+        <div class="float-end">
+            <p>Buatan <span class="text-danger"></i></span>
+                by K-ONE</a></p>
+        </div>
+    </div>
+</footer>
